e7cad59d4017ff3a4238d3fc8c2eb243574814c9
FisherDV <fisherd@mail.ru>
2021-09-22 23:42:49 +0500

    добавлены cors

--- ERCreator/ERCreator/settings.py
+++ ERCreator/ERCreator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',    
     'rest_framework_simplejwt',
 
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'ERCreator.urls'
@@ -139,3 +142,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
