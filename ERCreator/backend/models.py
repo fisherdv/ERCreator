b2597853557ee02172f583195a5d56653c53b8ac
FisherDV <fisherd@mail.ru>
2021-09-27 21:26:50 +0500

    change models

--- ERCreator/backend/models.py
+++ ERCreator/backend/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth import get_user_model
 
 class ERModel(models.Model):
     name = models.CharField(max_length=256)
-    comment = models.CharField(max_length=256, null=True, blank=True)    
-    # entities = models.ManyToManyField('Entity', blank=True)    
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="er_models")
+    comment = models.CharField(max_length=256, null=True, blank=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="er_models")
 
 
 class Entity(models.Model):
@@ -15,23 +15,26 @@ class Entity(models.Model):
     comment = models.CharField(max_length=256, null=True, blank=True)
     positionX = models.IntegerField(default="0")
     positionY = models.IntegerField(default="0")
-    er_model = models.ForeignKey('ERModel', on_delete=models.CASCADE, related_name="entities")
+    er_model = models.ForeignKey(
+        'ERModel', on_delete=models.CASCADE, related_name="entities")
 
 
-class Attribute(models.Model):    
+class Attribute(models.Model):
     name = models.CharField(max_length=256)
-    size = models.IntegerField(null=True)
-    default = models.CharField(max_length=256)
+    size = models.IntegerField(null=True, blank=True)
+    default = models.CharField(max_length=256, null=True, blank=True)
     comment = models.CharField(max_length=256, null=True, blank=True)
-    
+
     is_primary_key = models.BooleanField(default=False)
     is_unique = models.BooleanField(default=False)
     is_nullable = models.BooleanField(default=False)
     is_index = models.BooleanField(default=False)
-    
-    foreign_key = models.ForeignKey('Attribute', on_delete=models.SET_NULL, null=True, blank=True)
-    entity = models.ForeignKey('Entity', on_delete=models.CASCADE, related_name="attributes")
-    type = models.ForeignKey('Type', on_delete=models.PROTECT)
+
+    foreign_key = models.ForeignKey(
+        'Attribute', on_delete=models.SET_NULL, null=True, blank=True)
+    entity = models.ForeignKey(
+        'Entity', on_delete=models.CASCADE, related_name="attributes")
+    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=True, blank=True)
 
 
 class Type(models.Model):
